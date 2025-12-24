ebfe2f9984543ee7f068fd678ceaaa439000acbb
박재서 <wotjoo@naver.com>
2025-12-24 16:01:11 +0900

    24일 진짜진짜최종

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'accounts',
     'movies',
     'reviews',
-    'data',
     'boards',
 ]
 
--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -4,7 +4,6 @@ from .views import (
     MovieDetailAPIView,
     MovieRecommendAPIView,
     MovieSearchAPIView,
-    MovieSearchAPIView,
     MovieTrailerAPIView,
     MovieCreditsAPIView,
     PersonDetailAPIView,
